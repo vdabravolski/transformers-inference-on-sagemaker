4e96f6b7db11df129b20f03075eb068d8b84c761
EC2 Default User <ec2-user@ip-172-16-52-130.ec2.internal>
2021-02-25 00:14:31 +0000

    adding profiling results for different dataloaders

--- README.md
+++ README.md
@@ -2,14 +2,17 @@
 A sample repo to show how to optimize NLP inference on Amazon SageMaker
 
 # Backlog
-1. Add inference code (rank aware) to perform classification pipeline - done, with issues.
-2. Add data loader (rank aware) to load the financial sentiment dataset - done, with issues.
+1. Add inference code (rank aware) to perform classification pipeline - done.
+2. Add data loader (rank aware) to load the financial sentiment dataset - done.
 3. Add support of SPOT instance (specifically, pay attention to checkpointing)
 4. Add relevant metrics (end-to-end inference time, inference cost, ???)
 5. Do testing/benchmarking on G4s (single-device and multi-device) and P3s (multi-gpu)
 
 
 ## Current issues:
-1. Need to replace default DistributedSampler with custom implementation (e.g. [this one](https://github.com/SeungjunNah/DeepDeblur-PyTorch/blob/master/src/data/sampler.py)) because default sampler augments batches to even out size across nodes. It's undesirable for inference.
-2. According to SM Profiler Report, GPU devices are underutilized. Need to dive deeper. We are using inference pipeline and loading it to specific device. Why it's not reflected in GPU Utilization? Can it be an issue with data loading? Try to use WANDB for profiling and compare it to SM Profiler.
-3. We are using DDP to spin up inference processes in multi-node/multi-gpu setup. DDP may have synchonization enforced even during forward passes. Explore [no_sync](https://github.com/pytorch/pytorch/blob/master/torch/nn/parallel/distributed.py#L656) context manager to avoid it. Do we need to wrap models with DDP at all? Right now, it's not the case. 
+1. One of samples in Amazon Review dataset is violating max_lenght=512, which causes failure. Need to restrict max_length for all dataset samples.
+2. After profiling of inference, we observered following behavior:
+- For dummy dataloader (always returns one sample stored in memory) we see that all GPUs are highy utilized;<br>
+![GPU Utilization with Dummy Dataset](static/dummy_gpu.png)<br>
+- For real dataloader using HuggingFace Dataset "amazon_polarity", only single GPU per node is fully utilized. Hence, we need to look into optimizing data loading.<br>
+![GPU Utilization with Amazon Polarity Dataset](static/amazon_reviews.png)<br>
--- src/batch_inference.py
+++ src/batch_inference.py
@@ -7,6 +7,9 @@ import torch.distributed as dist
 from launcher import get_training_world
 from torch.utils.data import DistributedSampler, DataLoader
 from datasets import load_dataset
+import wandb
+from custom_data_utils import DistributedEvalSampler, DummyDataset
+
 
 def _get_global_rank(local_rank):
     hosts = json.loads(os.environ["SM_HOSTS"])
@@ -14,6 +17,18 @@ def _get_global_rank(local_rank):
     num_gpus = int(os.environ["SM_NUM_GPUS"])
     
     return host_rank*num_gpus+local_rank
+
+def _get_dataset(local_rank, data_split, cache_dir=os.environ["SM_INPUT_DIR"], dummy_dataset=False, download_mode="reuse_dataset_if_exists"):
+    if dummy_dataset:
+        return DummyDataset()
+    
+    return load_dataset("amazon_polarity", cache_dir=cache_dir, download_mode=download_mode, split=data_split)
+
+def _setup_wandb(args):
+    if args.wandb_api_key is not None:
+        os.environ["WANDB_API_KEY"]=args.wandb_api_key
+        os.environ["WANDB_PROJECT"]=args.wandb_project
+        wandb.init()
     
 
 def main():
@@ -21,9 +36,13 @@ def main():
     parser.add_argument("--local_rank", type=int)
     parser.add_argument("--inference-batch", type=int, default=16)
     parser.add_argument("--data-split", type=str, default='test')
+    parser.add_argument("--dummy-dataset", type=lambda s: s.lower() in ['true', 't', 'yes', '1'], 
+                        default=False, help="whether to use dummy dataset or not")
+    parser.add_argument("--wandb-api-key", type=str, default=None)
+    parser.add_argument("--wandb-project", type=str, default=None)
     args = parser.parse_args()
     
-    print(f"Running inference stub in global rank={_get_global_rank(args.local_rank)}")
+    print(f"Running inference in process with global_rank={_get_global_rank(args.local_rank)} and local_rank={args.local_rank}")
     
     world = get_training_world()
     global_rank = _get_global_rank(args.local_rank)
@@ -35,23 +54,26 @@ def main():
     
     # Downloading model and tokenizer in one process. 
     if args.local_rank==0:
+        _setup_wandb(args)
         print("downloading model/tokenizer in local_rank=0 process")
         inference_pipeline = pipeline('sentiment-analysis', device=args.local_rank)
-        dataset = load_dataset("amazon_polarity", cache_dir=os.environ["SM_INPUT_DIR"], split=args.data_split)
+        dataset = _get_dataset(args.local_rank, args.data_split, dummy_dataset=args.dummy_dataset)
 
     # Other have to wait
     torch.distributed.barrier()
     if args.local_rank!=0:
         print("Initializing pre-downloaded model/tokenizer in local_rank!=0 processes")
         inference_pipeline = pipeline('sentiment-analysis', device=args.local_rank)
-        dataset = load_dataset("amazon_polarity", cache_dir=os.environ["SM_INPUT_DIR"], download_mode="reuse_cache_if_exists", split=args.data_split)
+        dataset = _get_dataset(args.local_rank, args.data_split, dummy_dataset=args.dummy_dataset, download_mode="reuse_cache_if_exists",)
     
-    sampler = DistributedSampler(dataset, num_replicas=world["size"], rank=global_rank) if world["size"]>1 else None
+    sampler = DistributedEvalSampler(dataset, num_replicas=world["size"], rank=global_rank) if world["size"]>1 else None
     loader = DataLoader(dataset, batch_size=args.inference_batch, shuffle=(sampler is None), sampler=sampler)
     
+    # Synchronizing across all processes before starting inference
+    torch.distributed.barrier()
     for batch_id, data in enumerate(loader):
         model_output = inference_pipeline(data['content'])
-        print(f"Dealing with batch id={batch_id} for rank={global_rank}. Model output={model_output}")
+        print(f"Ran inference for batch id={batch_id} in process with global_rank={global_rank}.")
     
 if __name__ == "__main__":
     main()
